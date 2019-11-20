82cb33ca5e1745ef03793f1c222137eb7892a1f4
danielenggaard <daniel@seos.dk>
2019-11-20 20:11:07 +0100

    Updates

---
+++ precache-manifest.d698be2271bf0989457c639e44fa7ce7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89466c5fca160da27d69e3226e8892e4",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "8bd1b34358c39b0cab28",
+    "url": "/snake/static/css/main.83cb1293.chunk.css"
+  },
+  {
+    "revision": "97f91404c08a9a8d87c6",
+    "url": "/snake/static/js/2.703375ce.chunk.js"
+  },
+  {
+    "revision": "8bd1b34358c39b0cab28",
+    "url": "/snake/static/js/main.44784a40.chunk.js"
+  },
+  {
+    "revision": "9be3ef3518fdb1ce2533",
+    "url": "/snake/static/js/runtime-main.2ac187c7.js"
+  }
+]);
